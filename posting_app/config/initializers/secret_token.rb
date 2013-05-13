20e5dabfa33c6317e6322dc4c6e563c02cb86e86
Hakim Benbekhti <hakim.benbekhti@gmail.com>
2013-05-13 21:55:04 +0100

    first commit

---
+++ posting_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PostingApp::Application.config.secret_token = '4be4ee0972018342e5c95ae90acf3fca59d30c86b02eba0078d4400a4e12923de661b930ac3b29889bb1836c53f68e02adc79116af33134f34ddea14e37a25d7'
